b8cc93fbb29ccecf0fe85ef776731d2ab76d553f
S A U R A V <dwarmachine24@gmail.com>
2025-12-21 12:01:17 +0530

    Update tokay-vendor.mk

--- tokay-vendor.mk
+++ tokay-vendor.mk
@@ -561,7 +561,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt-astrea_20240329.00_RC02 \
     DevicePersonalizationPrebuiltPixel2024-bfinal_aiai_20250217.00_RC08 \
     DiagMon \
     EuiccGoogle \
